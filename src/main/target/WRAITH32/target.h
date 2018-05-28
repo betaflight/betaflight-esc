3bac356a35c2ddb62db95fab61b4e272a3fcfea0
Jacob Walser <jwalser90@gmail.com>
2018-05-28 09:55:59 -0400

    wraith32: new target

---
+++ src/main/target/WRAITH32/target.h
@@ -0,0 +1,40 @@
+#pragma once
+
+// PB4 --- BLUE
+#define LED0_GPIO       GPIOB
+#define LED0_PIN        LL_GPIO_PIN_4
+#define LED0_PERIPHERAL LL_AHB1_GRP1_PERIPH_GPIOB
+
+// PB5 --- RED
+#define LED1_GPIO       GPIOB
+#define LED1_PIN        LL_GPIO_PIN_5 // This pin goes to a test point. The red led is apparently not connected on the board used to test.
+#define LED1_PERIPHERAL LL_AHB1_GRP1_PERIPH_GPIOB
+
+// PB3 --- GREEN
+#define LED2_GPIO       GPIOB
+#define LED2_PIN        LL_GPIO_PIN_3
+#define LED2_PERIPHERAL LL_AHB1_GRP1_PERIPH_GPIOB
+
+// PA10 --- Phase_A_HI
+#define A_FET_HI_GPIO   GPIOA
+#define A_FET_HI_PIN    LL_GPIO_PIN_10
+
+// PA9 --- Phase_B_HI
+#define B_FET_HI_GPIO   GPIOA
+#define B_FET_HI_PIN    LL_GPIO_PIN_9
+
+// PA8 --- Phase_C_HI
+#define C_FET_HI_GPIO   GPIOA
+#define C_FET_HI_PIN    LL_GPIO_PIN_8
+
+// PB1 --- Phase_A_LO
+#define A_FET_LO_GPIO   GPIOB
+#define A_FET_LO_PIN    LL_GPIO_PIN_1
+
+// PB0 --- Phase_B_LO
+#define B_FET_LO_GPIO   GPIOB
+#define B_FET_LO_PIN    LL_GPIO_PIN_0
+
+// PA7 --- Phase_C_LO
+#define C_FET_LO_GPIO   GPIOA
+#define C_FET_LO_PIN    LL_GPIO_PIN_7
